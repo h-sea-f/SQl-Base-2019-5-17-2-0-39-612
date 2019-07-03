67388d2bf505c6e6c2ad535e7e1db18b5da28233
h-sea-f <h_sea_f@163.com>
2019-07-03 11:12:28 +0800

    7.3

---
+++ student_examination_system.sql
@@ -0,0 +1,11 @@
+CREATE TABLE student(id INT(10),NAME VARCHAR(10),age INT(10),sex VARCHAR(10));
+INSERT INTO student VALUES(001,'张三',18,'男');
+INSERT INTO student VALUES(002,'李四',20,'女');
+CREATE TABLE `subject`(id INT(10),`subject` VARCHAR(10),teacher VARCHAR(10),description VARCHAR(20));
+INSERT INTO `subject` VALUES(1001,'语文','王老师','本次考试比较简单');
+INSERT INTO `subject` VALUES(1002,'数学','刘老师','本次考试比较难');
+CREATE TABLE score(id INT(10),student_id INT(10),subject_id INT(10),score FLOAT);
+INSERT INTO score VALUES(1,001,1001,80);
+INSERT INTO score VALUES(2,002,1002,60);
+INSERT INTO score VALUES(3,001,1001,70);
+INSERT INTO score VALUES(4,002,1002,60.5);
